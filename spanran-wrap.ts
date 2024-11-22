8046c84790b5c704e427d9e16a5d4fd9b03808a9
Wes Bos <wesbos@gmail.com>
2024-11-22 08:49:45 -0500

    last min changes

--- spanran-wrap.ts
+++ spanran-wrap.ts
@@ -3,6 +3,11 @@ const titles = document.querySelectorAll<HTMLHeadingElement>('h1, h2');
 const wordSegmenter = new Intl.Segmenter([], { granularity: 'word' });
 const letterSegmenter = new Intl.Segmenter([], { granularity: 'grapheme' });
 
+
+function getRandomBetween(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 function wrapLettersInSpans(title: HTMLHeadingElement) {
   const text = title.innerText;
   const wordsSegments = Array.from(wordSegmenter.segment(text));
@@ -17,6 +22,9 @@ function wrapLettersInSpans(title: HTMLHeadingElement) {
       const letter = letterSegment.segment;
       const letterSpan = document.createElement('span');
       letterSpan.classList.add('letter');
+      letterSpan.style.setProperty('--left', getRandomBetween(-10, 10) + 'deg');
+      letterSpan.style.setProperty('--right', getRandomBetween(-10, 10) + 'deg');
+      // letterSpan.style.setProperty('animation-delay', (Math.random() * 5) + 'ms');
       letterSpan.innerText = letter;
       wordSpan.appendChild(letterSpan);
     }
